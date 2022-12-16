280285f9aa91db659262f13d27d342976b32aa8f
Jay Håkansson <jay@rackham2.uppmax.uu.se>
2022-12-16 12:00:54 +0100

    new input

--- scripts/exonerate_gene_fishing.sh
+++ scripts/exonerate_gene_fishing.sh
@@ -2,9 +2,9 @@
 #SBATCH -A snic2022-22-721
 #SBATCH -p core -n 2
 #SBATCH -t 4-00:00:00
-#SBATCH -J exonerate_gene_alignment
-#SBATCH -o /proj/sllstore2017063/y_chromosome/nobackup/logs/141222_exonerate_Y #write slurm log name here
-#SBATCH -e /proj/sllstore2017063/y_chromosome/nobackup/logs/141222_exonerate_Y
+#SBATCH -J Y_spec_exonerate_gene_alignment
+#SBATCH -o /proj/sllstore2017063/y_chromosome/nobackup/logs/161222_exonerate_Y_spec_genes_best5 #write slurm log name here
+#SBATCH -e /proj/sllstore2017063/y_chromosome/nobackup/logs/161222_exonerate_Y_spec_genes_best5
 #SBATCH --mail-user jay.hakansson.4449@student.uu.se
 #SBATCH --mail-type=FAIL,END
 
@@ -12,6 +12,8 @@ ASSEMBLY=/proj/sllstore2017063/y_chromosome/rawdata/O11_data/O11.fasta
 GENES=/proj/sllstore2017063/y_chromosome/nobackup/results/D_paulistorum_O11_res/gene_fishing/genes.fasta
 RESULT=/proj/sllstore2017063/y_chromosome/nobackup/results/D_paulistorum_O11_res/03_exonerate_gene_fishing/
 Y_ASSEMBLY=/proj/sllstore2017063/y_chromosome/nobackup/results/D_paulistorum_O11_res/05_assemblies/flye_assembler_nanopore/assembly.fasta
+Y_SPEC_GENES=/proj/sllstore2017063/y_chromosome/nobackup/results/D_paulistorum_O11_res/03_exonerate_gene_fishing/Ygenes_Dwil_also_in_Dmel.fasta
+
 
 module load bioinfo-tools exonerate
 
@@ -27,5 +29,16 @@ cd $RESULT
 
 #exonerate --model affine:local --percent 25 --bestn 3 -Q dna -T dna -q $GENES -t $ASSEMBLY > 25_percent_genes_to_O11.txt
 
-exonerate --model affine:local --percent 25 --bestn 3 -Q dna -T dna -q $GENES -t $Y_ASSEMBLY > 25_percent_flye_Y_asm_gene_hits.txt
+#exonerate --model affine:local --percent 25 --bestn 3 -Q dna -T dna -q $GENES -t $Y_ASSEMBLY > 25_percent_flye_Y_asm_gene_hits.txt
+
+#filter on at least 10% of max score
+
+#exonerate --model affine:local --percent 10 --bestn 3 -Q dna -T dna -q $GENES -t $ASSEMBLY > 10_percent_genes_to_O11.txt
+
+#exonerate --model affine:local --percent 10 --bestn 3 -Q dna -T dna -q $GENES -t $Y_ASSEMBLY > 10_percent_flye_Y_asm_gene_hits.txt
+
+#alignment of (probably) Y-specific genes that exist in both willistoni and melanogaster
+exonerate --model affine:local --bestn 5 -Q dna -T dna -q $Y_SPEC_GENES -t $ASSEMBLY > Y_spec_genes_to_O11_best5.txt
+
+exonerate --model affine:local --bestn 5 -Q dna -T dna -q $Y_SPEC_GENES -t $Y_ASSEMBLY > Y_spec_genes_to_flye_Y_asm_best5.txt
 
