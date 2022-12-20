839f15c86a465d03307a9623500bf8561b8b318c
Jay Håkansson <jay.hakansson.4449@student.uu.se>
2022-12-20 11:57:17 +0100

    updated paths

--- scripts/01_read_mapping/minimap2_nanopore_to_O11asm.sh
+++ scripts/01_read_mapping/minimap2_nanopore_to_O11asm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -l
 #SBATCH -A snic2022-22-721
-#SBATCH -p core -n 1
+#SBATCH -p core -n 2
 #SBATCH -t 2-00:00:00
 #SBATCH -J long_map
+#SBATCH -o /proj/sllstore2017063/y_chromosome/nobackup/logs/191222__map_long_canu
+#SBATCH -e /proj/sllstore2017063/y_chromosome/nobackup/logs/191222_map_long_canu
 #SBATCH --mail-user jay.hakansson.4449@student.uu.se
 #SBATCH --mail-type=FAIL,END
 
@@ -24,5 +26,5 @@ module load bioinfo-tools minimap2 samtools
 
 cd $OUTDIR_CANU
 
-minimap2 -x map-ont -d O11_long_index.mmi $O11_CANU                   
+#minimap2 -x map-ont -d O11_long_index.mmi $O11_CANU                   
 minimap2 -ax map-ont O11_long_index.mmi $reads | samtools view -@ 2 -b | samtools sort -o sorted_${NAME}.bam
