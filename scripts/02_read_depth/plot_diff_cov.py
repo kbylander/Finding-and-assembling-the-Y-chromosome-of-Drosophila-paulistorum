bf9ca13bb51ad4c0a4a765e7f034d8f87fe03b1a
kalle <98012682+kbylander@users.noreply.github.com>
2022-12-21 10:12:41 +0100

    Update plot_diff_cov.py

--- scripts/02_read_depth/plot_diff_cov.py
+++ scripts/02_read_depth/plot_diff_cov.py
@@ -1,26 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
-data=pd.read_csv("average_normalised_samples_2.out",usecols=[3,4,5],header=None,sep="\t")
+"""
+sys.argv[1] : data file
+sys.argv[2] : 1 png name
+sys.argv[3] : 2 png name
 
-plt.hist(data[5],bins=1000,color="r",label="Mean coverage ratio (Female & Mixed)")
-plt.title("Mean coverage ratio (Female & Mixed)")
-plt.ylabel("Occurences")
-plt.xlabel("Ratio (Female & Mixed)")
-plt.xlim(0,1.7)
-#plt.ylim(0,200)
-plt.show()
+"""
+if __name__ == "__main__":  
 
-#female
-plt.hist(data[3],bins=500,color="r",alpha=0.3,label="sum of female")
-plt.title("Sum of coverage between samples")
-plt.xlabel("Coverage")
-plt.ylabel("Occurences")
-plt.legend()
-plt.xlim(0,8000)
+    data=pd.read_csv(sys.argv[1],usecols=[3,4,5],header=None,sep="\t")
 
-#mixed
-plt.hist(data[4],bins=500,color="b",alpha=0.3,label="sum of mixed")
-plt.legend()
-plt.xlim(0,8000)
-plt.show()
+    plt.hist(data[5],bins=1000,color="r",label="Mean coverage ratio (Female & Mixed)")
+    plt.title("Mean coverage ratio (Female & Mixed)")
+    plt.ylabel("Occurences")
+    plt.xlabel("Ratio (Female & Mixed)")
+    plt.xlim(0,1.7)
+    #plt.ylim(0,200)
+    #plt.show()
+    plt.savefig(sys.argv[2])
+
+    #female
+    plt.hist(data[3],bins=2500,color="r",alpha=0.3,label="sum of female")
+    plt.title("Sum of coverage between samples")
+    plt.xlabel("Coverage")
+    plt.ylabel("Occurences")
+    plt.legend()
+    plt.xlim(0,11000)
+
+    #mixed
+    plt.hist(data[4],bins=2500,color="b",alpha=0.3,label="sum of mixed")
+    plt.legend()
+    plt.xlim(0,11000)
+    #plt.show()
+    plt.savefig(sys.argv[3])
+
